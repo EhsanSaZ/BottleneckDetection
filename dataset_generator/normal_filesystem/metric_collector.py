5cadc979ed8db4b6773d1ec34723380885258de5
ehsan saeedizade <e24saeedi@gmail.com>
2021-10-11 13:28:44 -0700

    Move collect_system_metrics function to utilities

--- dataset_generator/normal_filesystem/metric_collector.py
+++ dataset_generator/normal_filesystem/metric_collector.py
@@ -5,6 +5,8 @@ import subprocess
 import sys, traceback
 from subprocess import check_output
 
+from dataset_generator.utilities.system_metric_collector import collect_system_metrics
+
 src_ip = "127.0.0.1"
 dst_ip = "127.0.0.1"
 port_number = "50505"
@@ -61,61 +63,6 @@ def transfer_file(i):
         # if not line.decode("utf-8"):
         #     break
         strings.replace("\r", "\n")
-
-
-def collect_system_metrics(pid_str):
-    pid = int(pid_str.strip())
-
-    value_list = []
-    # create process to collect io metrics
-    try:
-        proc = Popen(['cat', '/proc/' + str(pid).strip() + '/io'], universal_newlines=True, stdout=PIPE)
-        res = proc.communicate()[0]
-        res_parts = res.split("\n")
-        for line in res_parts:
-            if len(line.strip()) > 0:
-                index = line.rfind(":")
-                value = int(line[index + 1:].strip())
-                value_list.append(value)
-    except:
-        print("io stat not possible")
-        traceback.print_exc()
-
-    # create process to collect stat metrics
-    try:
-        proc = Popen(['cat', '/proc/' + str(pid).strip() + '/stat'], universal_newlines=True, stdout=PIPE)
-        res = proc.communicate()[0]
-        res_parts = res.split(" ")
-        for line in res_parts:
-            if len(line.strip()) > 0:
-                try:
-                    value = int(line.strip())
-                    value_list.append(value)
-                except:
-                    # only convert numbers to int as pass error for other strings
-                    pass
-                    # traceback.print_exc()
-    except:
-        print("stat not possible")
-        traceback.print_exc()
-
-    # create process to collect cpu memory metrics
-    try:
-        proc = Popen(['ps', '-p', str(pid).strip(), '-o', '%cpu,%mem'], universal_newlines=True, stdout=PIPE)
-        res = proc.communicate()[0]
-        res_parts = res.split("\n")
-        for line in res_parts:
-            if len(line.strip()) > 0:
-                if "%CPU" not in line:
-                    parts = line.split(" ")
-                    for x in parts:
-                        if len(x.strip()) > 0:
-                            value_list.append(float(x))
-    except:
-        traceback.print_exc()
-        print("cpu mem is not possible")
-
-    return value_list
 
 
 def get_buffer_value():
