cfba2e60f8aded5c708257eb3a5fa9f06bee515d
ehsan saeedizade <e24saeedi@gmail.com>
2021-11-24 16:25:13 -0800

    update inject_single_anomaly.sh

--- dataset_generator/normal_filesystem/inject_single_anomaly.sh
+++ dataset_generator/normal_filesystem/inject_single_anomaly.sh
@@ -10,17 +10,25 @@ do
     if [ $wait_period -gt 36000 ];then
        break
     fi
+    
+    python3 metric_collector.py 0 &
+    sleep $main_sleep_time;
+    killall -9 python3;
+    killall -9 java;
+    wait_period=$(($wait_period+60));
+    sleep 5;
+    
+    
     if [ $((number%2)) -eq 0 ]
     then
-        # T ODO check if with or with out "" is true
-        stress-ng --vm-bytes "$(awk '/MemAvailable/{printf "%d\n", $2 * 0.98;}' < /proc/meminfo)"k --vm-keep -m 10  &
+        sudo tc qdisc add dev $ethernet_interface_name root netem delay 0.1ms 0.5ms distribution normal;
         sleep 5;
-        python3 metric_collector.py 35 &
+        python3 metric_collector.py 41 &
         sleep $main_sleep_time;
-        killall -9 stress-ng;
         killall -9 python3;
         killall -9 java;
         wait_period=$(($wait_period+60));
+        tc qdisc del dev $ethernet_interface_name root;
         sleep 5;
     fi
 done
