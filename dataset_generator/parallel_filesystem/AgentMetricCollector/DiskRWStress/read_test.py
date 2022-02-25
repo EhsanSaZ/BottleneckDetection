0a98b66b95e5481b78c64e62e09c6b09e0cf5f7e
ehsan saeedizade <e24saeedi@gmail.com>
2022-02-24 17:13:15 -0800

    Move read and write congestion code in DiskRWStress directory, Add getting base path of files as input from user

--- dataset_generator/parallel_filesystem/AgentMetricCollector/DiskRWStress/read_test.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/DiskRWStress/read_test.py
@@ -11,20 +11,21 @@ The argument is the number of threads
 """
 BUFFER_SIZE = 1024 * 512
 max_thread_name = 16
-src_path = "/home/esaeedizade/Desktop/diskReadStress/"
-
-if not src_path.endswith('/'):
-    src_path = src_path + "/"
+# src_path = "/home/esaeedizade/Desktop/diskReadStress/"
+#
+# if not src_path.endswith('/'):
+#     src_path = src_path + "/"
 
 
 class ReadThread(Thread):
 
-    def __init__(self, filename):
+    def __init__(self, filename, src_path):
         Thread.__init__(self)
         self.folder_name = filename
+        self.src_path = src_path
 
     def run(self):
-        all_files = glob.glob(src_path + self.folder_name + "/*")
+        all_files = glob.glob(self.src_path + self.folder_name + "/*")
         random.shuffle(all_files)
         # file_count = 0
         # proc = subprocess.run(['sudo ./clearCacheScript.sh'], universal_newlines=True, shell=True)
@@ -44,11 +45,16 @@ class ReadThread(Thread):
                 #     proc = subprocess.run(['sudo ./clearCacheScript.sh'], universal_newlines=True, shell=True)
 
 
-thread_number = int(sys.argv[1])
+src_path = str(sys.argv[1])
+thread_number = int(sys.argv[2])
+
+if not src_path.endswith('/'):
+    src_path = src_path + "/"
+
 start_time = time.time()
 threads = []
 for x in range(thread_number):
-    new_thread = ReadThread(str((x+1) % max_thread_name))
+    new_thread = ReadThread(str((x+1) % max_thread_name), src_path)
     new_thread.start()
     threads.append(new_thread)
 
--- dataset_generator/parallel_filesystem/AgentMetricCollector/DiskRWStress/write_test.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/DiskRWStress/write_test.py
@@ -11,25 +11,26 @@ This file is run by a bash script to put the disk under the stress of writing by
 The argument is the number of threads
 """
 
-base_path = '/home/esaeedizade/Desktop/diskWriteStress/'
-
-if not base_path.endswith('/'):
-    base_path = base_path + "/"
+# base_path = '/home/esaeedizade/Desktop/diskWriteStress/'
+#
+# if not base_path.endswith('/'):
+#     base_path = base_path + "/"
 
 
 class WriteThread(Thread):
 
-    def __init__(self, filename):
+    def __init__(self, filename, base_path):
         Thread.__init__(self)
         self.filename = filename
+        self.base_path = base_path
         # 1024 bytes data - 1KB
         self.chars = rnd = os.urandom(1024)
     def run(self):
         while True:
             # strategy 1:
-            proc = Popen(['touch', base_path + self.filename + ".txt"])
+            proc = Popen(['touch', self.base_path + self.filename + ".txt"])
             proc.communicate()
-            with open(base_path + self.filename + ".txt", 'wb', buffering=0) as f:
+            with open(self.base_path + self.filename + ".txt", 'wb', buffering=0) as f:
                 for i in range(1024):
                     f.write(self.chars)
                     # f.flush()
@@ -40,15 +41,20 @@ class WriteThread(Thread):
             # for i in range(1024):
             #     os.write(fd, m)
             # os.close(fd)
-            proc2 = Popen(['rm', base_path + self.filename + ".txt"])
+            proc2 = Popen(['rm', self.base_path + self.filename + ".txt"])
             proc2.communicate()
 
 
-thread_number = int(sys.argv[1])
+base_path = str(sys.argv[1])
+thread_number = int(sys.argv[2])
+
+if not base_path.endswith('/'):
+    base_path = base_path + "/"
+
 start_time = time.time()
 threads = []
 for x in range(thread_number):
-    new_thread = WriteThread(str(x))
+    new_thread = WriteThread(str(x), base_path)
     new_thread.start()
     threads.append(new_thread)
 
