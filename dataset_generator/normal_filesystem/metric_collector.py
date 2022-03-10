0e50208f2588c1811cea26d5185782345d9c5194
ehsan saeedizade <e24saeedi@gmail.com>
2022-03-10 09:50:43 -0800

    Refactor normal file system code- metric_collector.py

--- dataset_generator/normal_filesystem/metric_collector.py
+++ dataset_generator/normal_filesystem/metric_collector.py
@@ -5,14 +5,15 @@ from subprocess import PIPE, Popen
 import subprocess
 import sys, traceback
 from subprocess import check_output
-import re
 import psutil
 
 from NetworkStatistics.NetworkStatisticsLogCollector_ss import NetworkStatisticsLogCollectorSS
-from DiskStatistics.DiskStatisticsLogCollector import DiskStatisticsLogCollector
-from system_metric_collector import collect_system_metrics
-from buffer_value_collector import get_buffer_value
-from disk_stat_collector import get_disk_stat
+from AgentMetricCollector.statistics_log_collector import StatisticsLogCollector
+
+# from DiskStatistics.DiskStatisticsLogCollector import DiskStatisticsLogCollector
+# from dataset_generator.normal_filesystem.AgentMetricCollector.collectors.system_metric_collector import collect_system_metrics
+# from buffer_value_collector import get_buffer_value
+
 src_ip = "127.0.0.1"
 dst_ip = "134.197.94.98"
 port_number = "50505"
@@ -20,9 +21,9 @@ time_length = 3600  # one hour data
 drive_name = "sda"  # drive_name = "sda" "nvme0n1" "xvdf" can be checked with lsblk command on ubuntu
 
 # path to read file for transferring
-src_path = "/home/esaeedizade/Desktop/srcData/"
+src_path = "/home/esaeedizade/dataDir/srcData/"
 # path to save received transferred data
-dst_path = "/home/esaeedizade/Desktop/dstData/"
+dst_path = "/home/esaeedizade/dataDir/dstData/"
 start_time_global = time.time()
 # label_value normal = 0, more labeled can be checked from command bash file
 label_value = int(sys.argv[1])
@@ -79,7 +80,9 @@ def collect_stat():
     res = proc.communicate()[0]
     parts = res.split("\n")
     network_statistics_collector = NetworkStatisticsLogCollectorSS(dst_ip, port_number)
-    disk_statistics_collector = DiskStatisticsLogCollector(drive_name=drive_name, sector_conversion_fctr=2)
+    # disk_statistics_collector = DiskStatisticsLogCollector(drive_name=drive_name, sector_conversion_fctr=2)
+    statistics_collector = StatisticsLogCollector(disk_drive_name=drive_name, sector_conversion_fctr=2)
+
     for x in parts:
         if "lustre" in x:
             isparallel_file_system = True
@@ -92,7 +95,6 @@ def collect_stat():
         is_first_time = True
         time_diff = 0
         epoc_time = 0
-        has_transfer_started = False
         sleep_time = 1
         epoc_count = 0
         main_output_string = ""
@@ -104,7 +106,7 @@ def collect_stat():
                 break
             # If the pid is 0, then the transfer thread is not started yet or the global pid is not updated yet
             # So it is not possible to collect for system metrics, then just skip this iteration
-            if sender_process is None: # or pid == 0
+            if sender_process is None:  # or pid == 0
                 continue
             try:
                 if (is_first_time):
@@ -114,10 +116,12 @@ def collect_stat():
                 epoc_time += 1
 
                 if time_diff >= (.1 / sleep_time):
-                    system_value_list = collect_system_metrics(pid, sender_process)
-                    buffer_value_list = get_buffer_value()
-                    output_string = network_statistics_collector.get_log_str()
-                    output_string += "," + disk_statistics_collector.get_log_str()
+                    system_value_list = statistics_collector.collect_system_metrics(pid, sender_process)
+                    buffer_value_list = statistics_collector.get_buffer_value()
+
+                    output_string = str(time.time()) + ","
+                    output_string += network_statistics_collector.get_log_str()
+                    output_string += "," + statistics_collector.get_disk_statistics_log_str()
                     global label_value
                     for item in system_value_list:
                         output_string += "," + str(item)
@@ -151,6 +155,7 @@ def collect_stat():
                 traceback.print_exc()
             time.sleep(sleep_time)
 
+
 class fileWriteThread(threading.Thread):
     def __init__(self, metric_string, label_value):
         threading.Thread.__init__(self)
@@ -158,10 +163,11 @@ class fileWriteThread(threading.Thread):
         self.label_value = label_value
 
     def run(self):
-        output_file = open("./logs/dataset_"+str(self.label_value)+".csv","a+")
+        output_file = open("./logs/dataset_" + str(self.label_value) + ".csv", "a+")
         output_file.write(str(self.metric_string))
         output_file.flush()
         output_file.close()
+
 
 class statThread(threading.Thread):
     def __init__(self):
@@ -176,7 +182,6 @@ Path("./SimpleSenderLog").mkdir(parents=True, exist_ok=True)
 
 stat_thread = statThread()
 stat_thread.start()
-
 
 file_transfer_thread = FileTransferThread(str(0))
 file_transfer_thread.start()
