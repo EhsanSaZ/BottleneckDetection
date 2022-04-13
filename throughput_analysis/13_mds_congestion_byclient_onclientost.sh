24d7a4da337e4849393f511d6483ea0e4e0a21d8
ehsan <e24saeedi@gmail.com>
2022-04-13 16:08:40 -0700

    Add 13_mds_congestion_byclient_onclientost.sh

---
+++ throughput_analysis/13_mds_congestion_byclient_onclientost.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+user_name='root'
+receiver_remote_client_ip=10.10.2.2
+remote_client_ip=10.10.2.3
+
+sender_oss_server_ip=10.10.1.2
+receiver_oss_server_ip=10.10.1.3
+other_oss_server_ip=10.10.1.4
+
+main_sleep_time=60
+
+clear_all_caches() {
+    echo "Clearing cache on sender oss server";
+    ssh root@$sender_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on receiver oss server";
+    ssh root@$receiver_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on other oss server";
+    ssh root@$other_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+
+    echo "Clearing cache on receiver lustre client";
+    ssh root@$receiver_remote_client_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on remote lustre client";
+    ssh root@$remote_client_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on sender lustre client";
+    sync; echo 3 > /proc/sys/vm/drop_caches;
+}
+kill_all_java_processes(){
+#    killall -9  -u $user_name python3;
+    killall -9  -u $user_name java;
+
+#    ssh root@$receiver_remote_client_ip 'killall -9  -u root python3'
+    ssh root@$receiver_remote_client_ip 'killall -9  -u root java'
+}
+
+clear_all_caches
+label='mds_congestion_by_clients_on_other_ost_0'
+ssh root@$receiver_remote_client_ip "java /users/Ehsan/AgentMetricCollector/collectors/SimpleReceiver_per_second_thr_monitor.java /lustre/dstDataDir/dstData/ 50505 ${label}"&
+sleep 5;
+java /users/Ehsan/BottleneckDetection/dataset_generator/utilities/SimpleSender1.java $receiver_remote_client_ip 50505 /lustre/dataDir/srcData/ 0 &
+sleep $main_sleep_time;
+kill_all_java_processes
+killall -9  -u $user_name python3;
+
+congestions=([1]="mds_congestion_by_clients_on_other_ost_1" [2]="mds_congestion_by_clients_on_other_ost_2"
+[4]="mds_congestion_by_clients_on_other_ost_4" [6]="mds_congestion_by_clients_on_other_ost_6"
+[8]="mds_congestion_by_clients_on_other_ost_8" [10]="mds_congestion_by_clients_on_other_ost_10"
+[12]="mds_congestion_by_clients_on_other_ost_12" [14]="mds_congestion_by_clients_on_other_ost_14"
+[16]="mds_congestion_by_clients_on_other_ost_16" [32]="mds_congestion_by_clients_on_other_ost_32"
+[64]="mds_congestion_by_clients_on_other_ost_64" [96]="mds_congestion_by_clients_on_other_ost_96"
+[128]="mds_congestion_by_clients_on_other_ost_128")
+
+for i in "${!congestions[@]}"; do
+#  printf "%s\t%s\n" "$i" "${congestions[$i]}"
+  clear_all_caches
+  label=${congestions[$i]}
+  ssh root@$receiver_remote_client_ip "java /users/Ehsan/AgentMetricCollector/collectors/SimpleReceiver_per_second_thr_monitor.java /lustre/dstDataDir/dstData/ 50505 ${label}"&
+  sleep 5;
+  ssh root@$remote_client_ip "python3 /users/Ehsan/AgentMetricCollector/DiskRWStress/mds_test_2.py ${i} 0"
+  java /users/Ehsan/BottleneckDetection/dataset_generator/utilities/SimpleSender1.java $receiver_remote_client_ip 50505 /lustre/dataDir/srcData/ $i &
+  sleep $main_sleep_time;
+  kill_all_java_processes
+  ssh root@$remote_client_ip 'killall -9  -u root python3'
+  sleep 1;
+done
