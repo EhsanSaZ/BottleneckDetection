ddc13402eb7772056dea0ef554922110a810a399
ehsan <e24saeedi@gmail.com>
2022-04-13 17:52:38 -0700

    Add 15_network_anomaly_network_delay.sh

---
+++ throughput_analysis/15_network_anomaly_network_delay.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+user_name='root'
+receiver_remote_client_ip=10.10.2.2
+#remote_client_ip=10.10.2.3
+#other_oss_server_ip=10.10.1.4
+
+sender_oss_server_ip=10.10.1.2
+receiver_oss_server_ip=10.10.1.3
+
+ethernet_interface_name='p1p1'
+
+main_sleep_time=60
+
+clear_all_caches() {
+    echo "Clearing cache on sender oss server";
+    ssh root@$sender_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on receiver oss server";
+    ssh root@$receiver_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+#    echo "Clearing cache on other oss server";
+#    ssh root@$other_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+
+    echo "Clearing cache on receiver lustre client";
+    ssh root@$receiver_remote_client_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+#    echo "Clearing cache on remote lustre client";
+#    ssh root@$remote_client_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on sender lustre client";
+    sync; echo 3 > /proc/sys/vm/drop_caches;
+}
+kill_all_java_processes(){
+#    killall -9  -u $user_name python3;
+    killall -9  -u $user_name java;
+
+#    ssh root@$receiver_remote_client_ip 'killall -9  -u root python3'
+    ssh root@$receiver_remote_client_ip 'killall -9  -u root java'
+}
+
+clear_all_caches
+label='network_anomaly_network_delay_0'
+ssh root@$receiver_remote_client_ip "java /users/Ehsan/AgentMetricCollector/collectors/SimpleReceiver_per_second_thr_monitor.java /lustre/dstDataDir/dstData/ 50505 ${label}"&
+sleep 5;
+java /users/Ehsan/BottleneckDetection/dataset_generator/utilities/SimpleSender1.java $receiver_remote_client_ip 50505 /lustre/dataDir/srcData/ 0 &
+sleep $main_sleep_time;
+kill_all_java_processes
+killall -9  -u $user_name python3;
+sleep 1;
+
+label_general="network_anomaly_network_delay_"
+levels=([1]=0.1 [2]=0.5 [3]=1 [4]=2)
+
+for i in "${!levels[@]}"; do
+#  printf "%s\t%s\n" "$i" "${levels[$i]}"
+  clear_all_caches
+  label=${label_general}${levels[$i]}
+  echo $label
+  ssh root@$receiver_remote_client_ip "java /users/Ehsan/AgentMetricCollector/collectors/SimpleReceiver_per_second_thr_monitor.java /lustre/dstDataDir/dstData/ 50505 ${label}"&
+  sudo tc qdisc add dev $ethernet_interface_name root netem delay 0.1ms ${levels[$i]}ms distribution normal;
+  sleep 5;
+  java /users/Ehsan/BottleneckDetection/dataset_generator/utilities/SimpleSender1.java $receiver_remote_client_ip 50505 /lustre/dataDir/srcData/ $i &
+  sleep $main_sleep_time;
+  kill_all_java_processes
+  tc qdisc del dev $ethernet_interface_name root;
+  sleep 1;
+done
