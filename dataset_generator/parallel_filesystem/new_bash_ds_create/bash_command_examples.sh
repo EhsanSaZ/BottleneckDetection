e3375681a90f42a929e3bc4410f80a2e33f65358
ehsan saeedizade <e24saeedi@gmail.com>
2022-04-19 11:43:49 -0700

    Add bash_command_examples.sh

---
+++ dataset_generator/parallel_filesystem/new_bash_ds_create/bash_command_examples.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#run a command in remote or local host and get pid pf the process created by that command
+#var=$(ssh root@node1 "/bin/bash -c 'java /users/Ehsan/AgentMetricCollector/collectors/SimpleReceiver_per_second_thr_monitor.java /lustre/dstDataDir/dstData/ 50505 testii > /dev/null & echo \$!'")
+#echo $var
+#java /users/Ehsan/BottleneckDetection/dataset_generator/utilities/SimpleSender1.java 10.10.2.2 50505 /lustre/dataDir/srcData/ 0
