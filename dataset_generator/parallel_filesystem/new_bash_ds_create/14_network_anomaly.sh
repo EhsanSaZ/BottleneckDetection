4baadafa94219bd7e6109d2f6d59bab63948d88d
ehsan saeedizade <e24saeedi@gmail.com>
2022-04-20 19:03:12 -0700

    Add 14, 15, 16, 17, 18_network_anomaly.sh

---
+++ dataset_generator/parallel_filesystem/new_bash_ds_create/14_network_anomaly.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+round_counter=1
+max_round_number=2
+main_sleep_time=60
+
+user_name='root'
+
+receiver_remote_client_ip=10.10.2.2
+remote_client_ip=10.10.2.3
+
+sender_oss_server_ip=10.10.1.2
+receiver_oss_server_ip=10.10.1.3
+
+ethernet_interface_name='p1p1'
+
+clear_all_caches() {
+    echo "Clearing cache on sender oss server";
+    ssh root@$sender_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on receiver oss server";
+    ssh root@$receiver_oss_server_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+
+    echo "Clearing cache on receiver lustre client";
+    ssh root@$receiver_remote_client_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on remote lustre client";
+    ssh root@$remote_client_ip 'sync; echo 3 > /proc/sys/vm/drop_caches';
+    echo "Clearing cache on sender lustre client";
+    sync; echo 3 > /proc/sys/vm/drop_caches;
+}
+
+kill_all_java_python3_processes(){
+    killall -9  -u $user_name python3;
+    killall -9  -u $user_name java;
+
+    ssh root@$receiver_remote_client_ip 'killall -9  -u root python3'
+    ssh root@$receiver_remote_client_ip 'killall -9  -u root java'
+}
+
+# network_anomaly_network_loss
+#levels=([1]=0.05 [2]=0.1 [3]=0.5 [4]=1)
+levels=(["109"]=0.05 ["110"]=0.1 ["111"]=0.5 ["112"]=1)
+
+while true
+do
+    printf "\n"
+    echo "round number ${round_counter}"
+    if [ $round_counter -gt $max_round_number ];then
+       break
+    fi
+    for i in "${!levels[@]}"; do
+      clear_all_caches
+      label=$i
+      echo "Run the java server on receiver side and start metric collector agent";
+      ssh root@$receiver_remote_client_ip "python3 /users/Ehsan/AgentMetricCollector/remote_parallel_metric_collector.py -l ${label} -jsp 50505 -jtl ${label}"&
+      tc qdisc add dev $ethernet_interface_name root netem loss ${levels[$i]}%;
+      sleep 5;
+      echo "Start collecting metrics on sender side";
+      python3 ../parallel_metric_collector.py -l ${label}  -jsp 50505&
+      sleep $main_sleep_time;
+      kill_all_java_python3_processes
+      tc qdisc del dev $ethernet_interface_name root;
+      sleep 1;
+    done
+    round_counter=$(($round_counter+1));
+done
