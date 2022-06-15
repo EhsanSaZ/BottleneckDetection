1500ff379aecd3db5ebace77d618a0cafd5cd7d7
ehsan saeedizade <e24saeedi@gmail.com>
2022-06-15 12:32:57 -0700

    Change metric types to string in data_converter.py

--- dataset_generator/parallel_filesystem/AgentMetricCollector/data_converter.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/data_converter.py
@@ -6,40 +6,40 @@ class DataConverter:
         self.prefix = prefix
         self.file_system = file_system
         self.log_data_types = {1: 'float', 4: 'int'}
-        self.metrics_datatypes = {1: 'float', 2: 'string', 3: 'float', 4: 'float', 5: 'float', 6: 'float', 7: 'float',
-                                  8: 'float', 9: 'float', 10: 'float', 11: 'string', 12: 'float', 13: 'float',
-                                  14: 'float',
-                                  15: 'float', 16: 'float', 17: 'float', 18: 'float', 19: 'float', 20: 'float',
-                                  21: 'float',
-                                  22: 'float', 23: 'float', 24: 'float', 25: 'float', 26: 'float', 27: 'float',
-                                  28: 'float',
-                                  29: 'float', 30: 'int', 31: 'int', 32: 'int', 33: 'int', 34: 'int', 35: 'int',
-                                  36: 'int',
-                                  37: 'int', 38: 'int', 39: 'int', 40: 'int', 41: 'int', 42: 'int', 43: 'int',
-                                  44: 'int',
-                                  45: 'int', 46: 'int', 47: 'int', 48: 'int', 49: 'int', 50: 'int', 51: 'int',
-                                  52: 'int',
-                                  53: 'int', 54: 'int', 55: 'int', 56: 'int', 57: 'int', 58: 'int',
-                                  59: 'intonly', 60: 'intonly', 61: 'intonly', 62: 'intonly', 63: 'int', 64: 'int',
-                                  65: 'int',
-                                  66: 'int', 67: 'int', 68: 'int', 69: 'float', 70: 'int', 71: 'int', 72: 'int',
-                                  73: 'int',
-                                  74: 'int', 75: 'int', 76: 'int', 77: 'int', 78: 'int', 79: 'int', 80: 'int',
-                                  81: 'int',
-                                  82: 'int', 83: 'int', 84: 'int', 85: 'int', 86: 'int', 87: 'float', 88: 'float',
-                                  89: 'int',
-                                  90: 'int', 91: 'int', 92: 'int', 93: 'int', 94: 'int', 95: 'int', 96: 'int',
-                                  97: 'int',
-                                  98: 'int', 99: 'int', 100: 'int', 101: 'int', 102: 'int', 103: 'int', 104: 'int',
-                                  105: 'int',
-                                  106: 'int', 107: 'int', 108: 'int', 109: 'int', 110: 'int', 111: 'int', 112: 'int',
-                                  113: 'int', 114: 'int', 115: 'int', 116: 'int', 117: 'int', 118: 'int', 119: 'int',
-                                  120: 'int', 121: 'int', 122: 'int', 123: 'int', 124: 'int', 125: 'int', 126: 'int',
-                                  127: 'int', 128: 'int', 129: 'int', 130: 'int', 131: 'int', 132: 'int', 133: 'int',
-                                  134: 'int', 135: 'int', 136: 'int', 137: 'int', 138: 'int', 139: 'int', 140: 'int',
-                                  141: 'int', 142: 'int', 143: 'int', 144: 'int', 145: 'int', 146: 'int', 147: 'int',
-                                  148: 'float', 149: 'float', 150: 'float', 151: 'float',
-                                  152: 'int', 153: 'int'}
+        self.metrics_datatypes = {1: 'string', 2: 'string', 3: 'string', 4: 'string', 5: 'string', 6: 'string', 7: 'string',
+                                  8: 'string', 9: 'string', 10: 'string', 11: 'string', 12: 'string', 13: 'string',
+                                  14: 'string',
+                                  15: 'string', 16: 'string', 17: 'string', 18: 'string', 19: 'string', 20: 'string',
+                                  21: 'string',
+                                  22: 'string', 23: 'string', 24: 'string', 25: 'string', 26: 'string', 27: 'string',
+                                  28: 'string',
+                                  29: 'string', 30: 'string', 31: 'string', 32: 'string', 33: 'string', 34: 'string', 35: 'string',
+                                  36: 'string',
+                                  37: 'string', 38: 'string', 39: 'string', 40: 'string', 41: 'string', 42: 'string', 43: 'string',
+                                  44: 'string',
+                                  45: 'string', 46: 'string', 47: 'string', 48: 'string', 49: 'string', 50: 'string', 51: 'string',
+                                  52: 'string',
+                                  53: 'string', 54: 'string', 55: 'string', 56: 'string', 57: 'string', 58: 'string',
+                                  59: 'intonly', 60: 'intonly', 61: 'intonly', 62: 'intonly', 63: 'string', 64: 'string',
+                                  65: 'string',
+                                  66: 'string', 67: 'string', 68: 'string', 69: 'string', 70: 'string', 71: 'string', 72: 'string',
+                                  73: 'string',
+                                  74: 'string', 75: 'string', 76: 'string', 77: 'string', 78: 'string', 79: 'string', 80: 'string',
+                                  81: 'string',
+                                  82: 'string', 83: 'string', 84: 'string', 85: 'string', 86: 'string', 87: 'string', 88: 'string',
+                                  89: 'string',
+                                  90: 'string', 91: 'string', 92: 'string', 93: 'string', 94: 'string', 95: 'string', 96: 'string',
+                                  97: 'string',
+                                  98: 'string', 99: 'string', 100: 'string', 101: 'string', 102: 'string', 103: 'string', 104: 'string',
+                                  105: 'string',
+                                  106: 'string', 107: 'string', 108: 'string', 109: 'string', 110: 'string', 111: 'string', 112: 'string',
+                                  113: 'string', 114: 'string', 115: 'string', 116: 'string', 117: 'string', 118: 'string', 119: 'string',
+                                  120: 'string', 121: 'string', 122: 'string', 123: 'string', 124: 'string', 125: 'string', 126: 'string',
+                                  127: 'string', 128: 'string', 129: 'string', 130: 'string', 131: 'string', 132: 'string', 133: 'string',
+                                  134: 'string', 135: 'string', 136: 'string', 137: 'string', 138: 'string', 139: 'string', 140: 'string',
+                                  141: 'string', 142: 'string', 143: 'string', 144: 'string', 145: 'string', 146: 'string', 147: 'string',
+                                  148: 'string', 149: 'string', 150: 'string', 151: 'string',
+                                  152: 'string', 153: 'string'}
         self.metrics_id_to_attr = {1: 'avg_rtt_value', 2: 'pacing_rate', 3: 'cwnd_rate',
                                    4: 'avg_retransmission_timeout_value',
                                    5: 'byte_ack', 6: 'seg_out', 7: 'retrans', 8: 'mss_value', 9: 'ssthresh_value',
@@ -142,12 +142,12 @@ class DataConverter:
 
     def _get_data_type(self, val, type_):
         if type_ == "string":
-            return float(self._get_mbps(val))
+            return str(self._get_mbps(val))
         elif type_ == "float":
             return float(val)
         elif type_ == "intonly":
             tmp_value = str(val).split(".")[0]
-            return int(tmp_value)
+            return str(tmp_value)
         elif type_ == "dict":
             pass
         else:
