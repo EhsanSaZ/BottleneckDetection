a44f3f676b0dc6b89eaa6729cd20cf49bda808f7
ehsan saeedizade <e24saeedi@gmail.com>
2022-06-22 16:48:46 -0700

    Convert integer messages to string format

--- dataset_generator/parallel_filesystem/AgentMetricCollector/ZMQMessages.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/ZMQMessages.py
@@ -1,4 +1,4 @@
 class Messages:
-    default_message = 0
-    start_publishing = 1
-    received_publishing_signal = 2
+    default_message = "0"
+    start_publishing = "1"
+    received_publishing_signal = "2"
