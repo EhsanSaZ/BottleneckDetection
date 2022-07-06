caee1a10073ee4f3ecc25877bad91e0e81845aef
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-06 15:46:11 -0700

    Add helper threads module

---
+++ dataset_generator/parallel_filesystem/helper_threads.py
@@ -0,0 +1,47 @@
+import threading
+import time
+
+import psutil
+
+from AgentMetricCollector.statistics_log_collector import StatisticsLogCollector
+import global_vars
+
+
+class globalMetricsMonitor(threading.Thread):
+    def __init__(self, sleep_time):
+        threading.Thread.__init__(self)
+        self.sleep_time = sleep_time
+        self.statics_collector = StatisticsLogCollector()
+
+    def run(self):
+        # global global_vars.system_buffer_value, global_vars.system_cpu_usage, global_vars.system_memory_usage
+        while True:
+            global_vars.system_cpu_usage = psutil.cpu_percent()
+            global_vars.system_memory_usage = psutil.virtual_memory().percent
+            global_vars.system_buffer_value = self.statics_collector.get_buffer_value()
+            time.sleep(self.sleep_time)
+
+
+class fileWriteThread(threading.Thread):
+    def __init__(self, metric_string, label_value):
+        threading.Thread.__init__(self)
+        self.metric_string = metric_string
+        self.label_value = label_value
+
+    def run(self):
+        output_file = open("./sender/logs/dataset_" + str(self.label_value) + ".csv", "a+")
+        output_file.write(str(self.metric_string))
+        output_file.flush()
+        output_file.close()
+
+
+class overheadFileWriteThread(threading.Thread):
+    def __init__(self, overhead_string):
+        threading.Thread.__init__(self)
+        self.overhead_string = overhead_string
+
+    def run(self):
+        output_file = open("./sender/overhead_logs/overhead_footprints.csv", "a+")
+        output_file.write(str(self.overhead_string))
+        output_file.flush()
+        output_file.close()
