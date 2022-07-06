c0a5dfb668b1aa8780b32b68c332f394044f8bc3
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-06 16:31:54 -0700

    Add transfer_manager base class

---
+++ dataset_generator/parallel_filesystem/transfer_manager.py
@@ -0,0 +1,10 @@
+class transferManager:
+    def __init__(self):
+        self.transfer_monitoring_threads_dict = {}
+
+    def add_new_monitoring_thread(self, pid):
+        pass
+
+    def stop_monitoring_thread(self, pid):
+        pass
+   
