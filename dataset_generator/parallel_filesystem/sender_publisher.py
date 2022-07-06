82f9ad890dabdf99f1ad08a6e99fd5e79953ce8a
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-06 16:03:10 -0700

    move sendToCloud class to sender_publisher.py module

---
+++ dataset_generator/parallel_filesystem/sender_publisher.py
@@ -0,0 +1,86 @@
+import json
+import threading
+import traceback
+import zmq
+import global_vars
+from AgentMetricCollector.ZMQMessages import Messages
+
+
+class sendToCloud(threading.Thread):
+    def __init__(self, server_host, server_port,
+                 frontend_socket_ip_sender, frontend_socket_port_sender,
+                 frontend_socket_ip_receiver, frontend_socket_port_receiver,
+                 backend_socket_name, zmq_context, receiver_signaling_port):
+        super().__init__()
+        self.server_host = server_host
+        self.server_port = server_port
+        self.xpub_frontend_socket_ip_sender = frontend_socket_ip_sender
+        self.xpub_frontend_socket_port_sender = frontend_socket_port_sender
+
+        self.xpub_frontend_socket_ip_receiver = frontend_socket_ip_receiver
+        self.xpub_frontend_socket_port_receiver = frontend_socket_port_receiver
+
+        self.xsub_backend_socket_name = backend_socket_name
+        self.context = zmq_context
+        self.receiver_signaling_port = receiver_signaling_port
+        
+    def run(self):
+        xpub_frontend_socket = None
+        xsub_backend_socket = None
+        try:
+            # global ready_to_publish
+            rq_socket = self.context.socket(zmq.REQ)
+            rq_socket.connect("tcp://{}:{}".format(self.server_host, self.server_port))
+            #  we should have a mechanism to agree on sender and receiver ip port for publishing // reading from config file..
+            #  and notify receiver agents to when to start publishing
+            #  TODO and agree on transfer id
+            rq = {"request_type": "new_publisher_info",
+                  "data": {
+                      "sender": {
+                          "ip": self.xpub_frontend_socket_ip_sender,
+                          "port": self.xpub_frontend_socket_port_sender
+                      },
+                      "receiver": {
+                          "ip": self.xpub_frontend_socket_ip_receiver,
+                          "port": self.xpub_frontend_socket_port_receiver
+                      }
+                  }}
+            rq_socket.send_json(rq)
+            message = rq_socket.recv_json()
+            # message = {"response_code": "200"}
+            if message["response_code"] == "200":
+                print(f"Monitoring agent is registered successfully. Received reply [ {message} ]")
+                signaling_socket = self.context.socket(zmq.REQ)
+                signaling_socket.connect(
+                    "tcp://{}:{}".format(self.xpub_frontend_socket_ip_receiver, self.receiver_signaling_port))
+                signaling_socket.send_string(Messages.start_publishing)
+                signal_message = signaling_socket.recv_string()
+                if signal_message == Messages.received_publishing_signal:
+                    print("Receiver received publish signal")
+                xpub_frontend_socket = self.context.socket(zmq.XPUB)
+                xpub_frontend_socket.bind("tcp://*:{}".format(self.xpub_frontend_socket_port_sender))
+
+                xsub_backend_socket = self.context.socket(zmq.XSUB)
+                # xsub_backend_socket.bind("tcp://*:{}".format(xsub_backend_socket_port))
+                xsub_backend_socket.bind("inproc://{}".format(self.xsub_backend_socket_name))
+                global_vars.ready_to_publish = True
+
+                zmq.proxy(xpub_frontend_socket, xsub_backend_socket)
+            else:
+                print(f"Error in registering monitoring agent publisher socket. Received reply [ {message} ]")
+            # context.term()
+        except Exception as e:
+            traceback.print_exc()
+        # We never get here if everything is ok…
+        if xpub_frontend_socket:
+            un_sub_rq = {"request_type": "unsubscribe_publisher_info",
+                         "data": {
+                             "sender": {
+                                 "ip": self.xpub_frontend_socket_ip_sender,
+                                 "port": self.xpub_frontend_socket_port_sender
+                             }
+                         }}
+            xpub_frontend_socket.send_json(json.dumps(un_sub_rq))
+            xpub_frontend_socket.close()
+        if xsub_backend_socket:
+            xsub_backend_socket.close()
