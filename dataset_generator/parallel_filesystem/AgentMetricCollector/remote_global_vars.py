e8afd40cbdee37a9aac8dee3c4d97e46c67ec7a4
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-08 17:19:56 -0700

    Add label value to remote_global_vars.py

--- dataset_generator/parallel_filesystem/AgentMetricCollector/remote_global_vars.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/remote_global_vars.py
@@ -1,8 +1,9 @@
-mdt_parent_path = ""
-ready_to_publish = False
-should_run = True
 pid = 0
+label_value = None
 server_process = None
+ready_to_publish = False
+mdt_parent_path = ""
+should_run = True
 receiver_monitor_agent_pid = 0
 receiver_monitor_agent_process = None
 
