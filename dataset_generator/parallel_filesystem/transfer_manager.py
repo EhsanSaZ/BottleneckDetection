d9c223d95a9c318a22bd5cf22164fe2e2ea2ae88
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-13 13:52:26 -0700

    Refactor transfer manager - use new stat collector thread

--- dataset_generator/parallel_filesystem/transfer_manager.py
+++ dataset_generator/parallel_filesystem/transfer_manager.py
@@ -1,15 +1,16 @@
-from sender_stat_collector_thread import SenderStatThread
-from receiver_stat_collector_thread import ReceiverStatThread
+from stat_collector_thread import StatThread
+# from receiver_stat_collector_thread import ReceiverStatThread
 
 
 class TransferManager:
     def __init__(self, zmq_context, xsub_backend_socket_name, remote_ost_index_to_ost_agent_address_dict,
-                 src_path, mdt_parent_path, label_value):
+                 read_path, write_path, mdt_parent_path, label_value):
         self.transfer_monitoring_threads_dict = {}
         self.context = zmq_context
         self.xsub_backend_socket_name = xsub_backend_socket_name
         self.remote_ost_index_to_ost_agent_address_dict = remote_ost_index_to_ost_agent_address_dict
-        self.src_path = src_path
+        self.read_path = read_path
+        self.write_path = write_path
         self.mdt_parent_path = mdt_parent_path
         self.label_value = label_value
 
@@ -20,10 +21,11 @@ class TransferManager:
         source_port = transfer_info["local_port"]
         destination_ip = transfer_info["peer_ip"]
         destination_port = transfer_info["peer_port"]
-        thread = SenderStatThread(source_ip, source_port, destination_ip, destination_port,
-                                  self.context, self.xsub_backend_socket_name,
-                                  self.remote_ost_index_to_ost_agent_address_dict, str(pid),
-                                  self.src_path, self.mdt_parent_path, self.label_value)
+        thread = StatThread(source_ip, source_port, destination_ip, destination_port,
+                            self.context, self.xsub_backend_socket_name,
+                            self.remote_ost_index_to_ost_agent_address_dict, str(pid),
+                            self.read_path, self.mdt_parent_path, self.label_value,
+                            1, "./sender/logs/dataset_", "./sender/overhead_logs/overhead_footprints.csv")
         self.transfer_monitoring_threads_dict[pid] = thread
         thread.start()
 
@@ -33,12 +35,13 @@ class TransferManager:
         source_port = transfer_info["local_port"]
         destination_ip = transfer_info["peer_ip"]
         destination_port = transfer_info["peer_port"]
-        thread = ReceiverStatThread(source_ip, source_port, destination_ip, destination_port,
+        thread = StatThread(source_ip, source_port, destination_ip, destination_port,
                                     self.context, self.xsub_backend_socket_name,
                                     self.remote_ost_index_to_ost_agent_address_dict, str(pid),
-                                    self.src_path, self.mdt_parent_path, self.label_value)
+                                    self.write_path, self.mdt_parent_path, self.label_value,
+                            0, "./receiver/logs/dataset_", "./receiver/overhead_logs/overhead_footprints.csv")
         self.transfer_monitoring_threads_dict[pid] = thread
-        thread.start()
+        # thread.start()
 
     def stop_monitoring_thread(self, transfer_info):
         # print(transfer_info)
