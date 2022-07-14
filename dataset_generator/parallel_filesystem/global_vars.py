eee47d9248bd148abd3b6be2cca2f352443418d4
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-13 17:36:52 -0700

    Update global_vars

--- dataset_generator/parallel_filesystem/global_vars.py
+++ dataset_generator/parallel_filesystem/global_vars.py
@@ -1,9 +1,10 @@
 pid = None
 label_value = None
 sender_process = None
+server_process = None
 ready_to_publish = False
 mdt_parent_path = ""
 should_run = True
-sender_monitor_agent_pid = 0
-sender_monitor_agent_process = None
+monitor_agent_pid = 0
+monitor_agent_process = None
 
