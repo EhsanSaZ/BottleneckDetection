9d700b4ce7f61330569c4544852512fcf9d18f47
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-13 17:37:35 -0700

    Remove unused modules

--- dataset_generator/parallel_filesystem/AgentMetricCollector/remote_global_vars.py
+++
@@ -1,9 +0,0 @@
-pid = 0
-label_value = None
-server_process = None
-ready_to_publish = False
-mdt_parent_path = ""
-should_run = True
-receiver_monitor_agent_pid = 0
-receiver_monitor_agent_process = None
-
