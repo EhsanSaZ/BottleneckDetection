9dce75a97641bc9253075e762da612f4b78c218b
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-15 15:36:04 -0700

    Use regular expression to get mdt_path_info

--- dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/file_mdc_path_info.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/file_mdc_path_info.py
@@ -1,4 +1,5 @@
 from subprocess import Popen, PIPE
+import re
 
 
 class FileMdcPathInfo:
@@ -15,6 +16,8 @@ class FileMdcPathInfo:
             if len(line.strip()) > 0:
                 for mnt_path in lustre_mnt_point_list:
                     if mnt_path in line:
+                        re_pattern = r"(?P<info_part>.* )(?P<mdt_dir_name>(?P<lustre_name>.*)(?P<mdt_str>-MDT\d\d\d\d)-.*)"
+
                         slash_index = line.rfind(">")
                         file_name = line[slash_index + 1:].strip()
 
@@ -34,20 +37,28 @@ class FileMdcPathInfo:
                             parts = res.split("\n")
                             mdt_str = "-MDT" + hex_mdt_number
                             for x in range(1, len(parts)):
-                                mdt_name_parts = parts[x].split(" ")
-                                for part in mdt_name_parts:
-                                    first_dash_index = part.find("-")
-                                    if first_dash_index != -1 and part[
-                                                                  0:first_dash_index] in file_mount_point and mdt_str in part and "MDT" in part:
-                                        first_dash_index = part.find("-")
-                                        second_dash_index = part.find("-", first_dash_index + 1)
-
-                                        first_part = part[:first_dash_index]
-                                        second_part = part[second_dash_index + 1:]
-
-                                        mdt_dir_name = first_part + mdt_str + "-" + second_part
-                                        mdt_path = '/sys/kernel/debug/lustre/mdc/' + mdt_dir_name
-                                        return mdt_path, mdt_dir_name
-
+                                match = re.search(re_pattern, parts[i])
+                                if match:
+                                    gp_dict = match.groupdict()
+                                    match_mdt_dir_name = gp_dict.get("mdt_dir_name") or ""
+                                    match_lustre_name = gp_dict.get("lustre_name") or ""
+                                    match_mdt_str = gp_dict.get("mdt_str") or ""
+                                    if match_lustre_name in file_mount_point and mdt_str == match_mdt_str and match_mdt_dir_name != "":
+                                        mdt_path = '/sys/kernel/debug/lustre/mdc/' + match_mdt_dir_name
+                                        return mdt_path, match_mdt_dir_name
+                                # mdt_name_parts = parts[x].split(" ")
+                                # for part in mdt_name_parts:
+                                #     first_dash_index = part.find("-")
+                                #     if first_dash_index != -1 and part[
+                                #                                   0:first_dash_index] in file_mount_point and mdt_str in part and "MDT" in part:
+                                #         first_dash_index = part.find("-")
+                                #         second_dash_index = part.find("-", first_dash_index + 1)
+                                #
+                                #         first_part = part[:first_dash_index]
+                                #         second_part = part[second_dash_index + 1:]
+                                #
+                                #         mdt_dir_name = first_part + mdt_str + "-" + second_part
+                                #         mdt_path = '/sys/kernel/debug/lustre/mdc/' + mdt_dir_name
+                                #         return mdt_path, mdt_dir_name
                         else:
                             return ""
