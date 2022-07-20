e972ec388fa63ff4d54e4d8cc5ff2e4b182ee999
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-20 12:44:53 -0700

    Refactor statistics_log_collector.py to use new collector APIs

--- dataset_generator/parallel_filesystem/AgentMetricCollector/statistics_log_collector.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/statistics_log_collector.py
@@ -1,4 +1,4 @@
-try :
+try:
     from collectors.buffer_value_stat_collector import BufferValueStatCollector
     from collectors.system_metric_collector import SystemMetricCollector
     from collectors.file_osc_path_info import FileOscPathInfo
@@ -6,6 +6,7 @@ try :
     from collectors.client_ost_stat_collector import ClientProcessOstStat
     from collectors.client_mdt_stat_collector import ClientGetMdtStat
     from collectors.lustre_ost_stat_collector import LustreOstStatCollector
+    from collectors.network_statistics_log_collector_ss_v2 import NetworkStatisticsLogCollectorSS_V2
 except ModuleNotFoundError:
     from .collectors.buffer_value_stat_collector import BufferValueStatCollector
     from .collectors.system_metric_collector import SystemMetricCollector
@@ -14,36 +15,47 @@ except ModuleNotFoundError:
     from .collectors.client_ost_stat_collector import ClientProcessOstStat
     from .collectors.client_mdt_stat_collector import ClientGetMdtStat
     from .collectors.lustre_ost_stat_collector import LustreOstStatCollector
+    from .collectors.network_statistics_log_collector_ss_v2 import NetworkStatisticsLogCollectorSS_V2
 
 
 class StatisticsLogCollector:
-    def __init__(self, prefix=""):
+    def __init__(self, source_ip, source_port, destination_ip, destination_port, prefix=""):
+        self.network_statistics_collector = NetworkStatisticsLogCollectorSS_V2(source_ip, source_port, destination_ip,
+                                                                               destination_port, prefix)
         self.collect_system_metrics_obj = SystemMetricCollector(prefix)
-        self.buffer_value_stat_collector_obj = BufferValueStatCollector()
+        self.buffer_value_stat_collector_obj = BufferValueStatCollector(prefix)
         self.file_osc_path_info_obj = FileOscPathInfo()
         self.file_mdc_path_info_obj = FileMdcPathInfo()
-        self.process_ost_stat_obj = ClientProcessOstStat()
-        self.get_mdt_stat_obj = ClientGetMdtStat()
+        self.process_ost_stat_obj = ClientProcessOstStat(prefix)
+        self.get_mdt_stat_obj = ClientGetMdtStat(prefix)
         self.lustre_ost_stat_collector_obj = LustreOstStatCollector()
 
+    def collect_network_metrics(self):
+        self.network_statistics_collector.collect_metrics()
+        return self.network_statistics_collector.get_metrics_list()
+    
     def collect_system_metrics(self, pid_str, target_process):
-        self.collect_system_metrics_obj.collect_system_metrics(pid_str, target_process)
+        self.collect_system_metrics_obj.collect_metrics(pid_str, target_process)
         return self.collect_system_metrics_obj.get_metrics_list()
 
     def get_buffer_value(self):
-        return self.buffer_value_stat_collector_obj.get_buffer_value()
+        self.buffer_value_stat_collector_obj.collect_metrics()
+        return self.buffer_value_stat_collector_obj.get_metrics_list()
 
     def collect_file_ost_path_info(self, pid, src_path):
         return self.file_osc_path_info_obj.collect_file_ost_path_info(pid, src_path)
-    
+
     def collect_file_mdt_path_info(self, pid, src_path):
         return self.file_mdc_path_info_obj.collect_file_mdt_path_info(pid, src_path)
 
-    def process_ost_stat(self, ost_path, ost_dir_name, ost_stat_so_far=None):
-        return self.process_ost_stat_obj.process_ost_stat(ost_path, ost_dir_name, ost_stat_so_far)
-    
-    def get_mdt_stat(self, mdt_parent_path, mdt_dir_name, mdt_stat_so_far_dict=None):
-        return self.get_mdt_stat_obj.get_mdt_stat(mdt_parent_path, mdt_dir_name, mdt_stat_so_far_dict)
+    def process_ost_stat(self, ost_path, ost_dir_name):
+        self.process_ost_stat_obj.collect_metrics(ost_path, ost_dir_name)
+        return self.process_ost_stat_obj.get_metrics_list()
 
-    def process_lustre_ost_stats(self, st_agent_address, remote_ost_dir_name, remote_ost_stats_so_far=None):
-        return self.lustre_ost_stat_collector_obj.process_lustre_ost_stats(st_agent_address, remote_ost_dir_name, remote_ost_stats_so_far)
+    def get_mdt_stat(self, mdt_parent_path, mdt_dir_name):
+        self.get_mdt_stat_obj.collect_metrics(mdt_parent_path, mdt_dir_name)
+        return self.get_mdt_stat_obj.get_metrics_list()
+
+    def process_lustre_ost_stats(self, ost_agent_address, remote_ost_dir_name):
+        self.lustre_ost_stat_collector_obj.collect_metrics(ost_agent_address, remote_ost_dir_name)
+        return self.lustre_ost_stat_collector_obj.get_metrics_list()
