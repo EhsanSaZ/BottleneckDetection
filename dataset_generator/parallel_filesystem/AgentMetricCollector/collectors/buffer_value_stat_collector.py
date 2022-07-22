861bdf07ed619c5eba0f4e73a3a945c0bda63301
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-22 16:48:03 -0700

    Implement get_proto_message method for buffer_value_stat_collector.py

--- dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/buffer_value_stat_collector.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/buffer_value_stat_collector.py
@@ -1,17 +1,20 @@
 from subprocess import Popen, PIPE
+from google.protobuf.json_format import ParseDict, MessageToDict
 
 try:
     from abstract_collector import AbstractCollector
+    from protobuf_messages.buffer_value.buffer_value_metrics_pb2 import BufferValueMetrics
 except ModuleNotFoundError:
     from .abstract_collector import AbstractCollector
+    from .protobuf_messages.buffer_value.buffer_value_metrics_pb2 import BufferValueMetrics
 
 
 class BufferValueStatCollector(AbstractCollector):
     def __init__(self, prefix=""):
         super().__init__(prefix)
-        self.metrics_datatypes = {89: 'string', 90: 'string', 91: 'string', 92: 'string', 93: 'string', 94: 'string'}
-        self.metrics_id_to_attr = {89: 'tcp_rcv_buffer_min', 90: 'tcp_rcv_buffer_default', 91: 'tcp_rcv_buffer_max',
-                                   92: 'tcp_snd_buffer_min', 93: 'tcp_snd_buffer_default', 94: 'tcp_snd_buffer_max'}
+        self.metrics_datatypes = {1: 'string', 2: 'string', 3: 'string', 4: 'string', 5: 'string', 6: 'string'}
+        self.metrics_id_to_attr = {1: 'tcp_rcv_buffer_min', 2: 'tcp_rcv_buffer_default', 3: 'tcp_rcv_buffer_max',
+                                   4: 'tcp_snd_buffer_min', 5: 'tcp_snd_buffer_default', 6: 'tcp_snd_buffer_max'}
 
     def collect_metrics(self):
         self.get_buffer_value()
@@ -55,3 +58,16 @@ class BufferValueStatCollector(AbstractCollector):
             tmp_dict["{}{}".format(self.prefix, self.metrics_id_to_attr[keys_list[index]])] = self._get_data_type(
                 self.metrics_list[index], type_)
         self.metrics_dict = tmp_dict
+
+    def get_metrics_list_to_dict_no_prefix(self):
+        metrics_dict_no_prefix = {}
+        keys_list = list(self.metrics_id_to_attr.keys())
+        for index in range(len(self.metrics_list)):
+            type_ = self.metrics_datatypes[keys_list[index]]
+            metrics_dict_no_prefix[self.metrics_id_to_attr[keys_list[index]]] = self._get_data_type(
+                self.metrics_list[index], type_)
+        return metrics_dict_no_prefix
+
+    def get_proto_message(self):
+        message = ParseDict(self.get_metrics_list_to_dict_no_prefix(), BufferValueMetrics())
+        return message
