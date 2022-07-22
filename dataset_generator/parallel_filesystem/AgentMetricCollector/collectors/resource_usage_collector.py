fefcdddb655c55dedce857b930c5724ff10b642f
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-22 16:17:07 -0700

    Implement get_proto_message method for resource_usage_collector.py

--- dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/resource_usage_collector.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/resource_usage_collector.py
@@ -1,16 +1,19 @@
 import psutil
+from google.protobuf.json_format import ParseDict
 
 try:
     from abstract_collector import AbstractCollector
+    from protobuf_messages.resource_usage.resource_usage_metrics_pb2 import ResourceUsageMetrics
 except ModuleNotFoundError:
     from .abstract_collector import AbstractCollector
+    from .protobuf_messages.resource_usage.resource_usage_metrics_pb2 import ResourceUsageMetrics
 
 
 class ResourceUsageCollector(AbstractCollector):
     def __init__(self, prefix=""):
         super().__init__(prefix)
-        self.metrics_datatypes = {150: 'string', 151: 'string'}
-        self.metrics_id_to_attr = {150: 'system_cpu_percent', 151: 'system_memory_percent'}
+        self.metrics_datatypes = {1: 'string', 2: 'string'}
+        self.metrics_id_to_attr = {1: 'system_cpu_percent', 2: 'system_memory_percent'}
 
     def collect_metrics(self):
         self.get_cpu_mem()
@@ -39,3 +42,16 @@ class ResourceUsageCollector(AbstractCollector):
             tmp_dict["{}{}".format(self.prefix, self.metrics_id_to_attr[keys_list[index]])] = self._get_data_type(
                 self.metrics_list[index], type_)
         self.metrics_dict = tmp_dict
+
+    def get_metrics_list_to_dict_no_prefix(self):
+        metrics_dict_no_prefix = {}
+        keys_list = list(self.metrics_id_to_attr.keys())
+        for index in range(len(self.metrics_list)):
+            type_ = self.metrics_datatypes[keys_list[index]]
+            metrics_dict_no_prefix[self.metrics_id_to_attr[keys_list[index]]] = self._get_data_type(
+                self.metrics_list[index], type_)
+        return metrics_dict_no_prefix
+
+    def get_proto_message(self):
+        message = ParseDict(self.get_metrics_list_to_dict_no_prefix(), ResourceUsageMetrics())
+        return message
