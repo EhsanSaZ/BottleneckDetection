2524b0be02db90481f088ef91431013d6ec1e1aa
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-22 18:02:49 -0700

    Implement get_proto_message method for lustre_ost_metric_collector.py

--- dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/lustre_ost_metric_collector.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/lustre_ost_metric_collector.py
@@ -1,16 +1,19 @@
 import requests
+from google.protobuf.json_format import ParseDict, MessageToDict
 
 try:
     from abstract_collector import AbstractCollector
+    from protobuf_messages.lustre_ost.lustre_ost_metrics_pb2 import LustreOstMetrics
 except ModuleNotFoundError:
     from .abstract_collector import AbstractCollector
+    from .protobuf_messages.lustre_ost.lustre_ost_metrics_pb2 import LustreOstMetrics
 
 
 class LustreOstMetricCollector(AbstractCollector):
     def __init__(self, prefix=""):
         super().__init__(prefix)
-        self.metrics_datatypes = {152: 'string', 153: 'string'}
-        self.metrics_id_to_attr = {152: 'remote_ost_read_bytes', 153: 'remote_ost_write_bytes'}
+        self.metrics_datatypes = {1: 'string', 2: 'string'}
+        self.metrics_id_to_attr = {1: 'remote_ost_read_bytes', 2: 'remote_ost_write_bytes'}
         self.all_remote_ost_stats_so_far = {}
 
     def collect_metrics(self, ost_agent_address, remote_ost_dir_name):
@@ -32,8 +35,10 @@ class LustreOstMetricCollector(AbstractCollector):
                     if tokens[0] == "read_bytes" or tokens[0] == "write_bytes":
                         remote_ost_stats_latest_value[tokens[0]] = float(tokens[len(tokens) - 1])
 
-            value_list.append(float((remote_ost_stats_latest_value.get("read_bytes") or 0) - (remote_ost_stats_so_far.get("read_bytes") or 0)))
-            value_list.append(float((remote_ost_stats_latest_value.get("write_bytes") or 0) - (remote_ost_stats_so_far.get("write_bytes") or 0)))
+            value_list.append(float((remote_ost_stats_latest_value.get("read_bytes") or 0) - (
+                        remote_ost_stats_so_far.get("read_bytes") or 0)))
+            value_list.append(float((remote_ost_stats_latest_value.get("write_bytes") or 0) - (
+                        remote_ost_stats_so_far.get("write_bytes") or 0)))
             # return value_list, remote_ost_stats_latest_value
         else:
             value_list = [0.0, 0.0]
@@ -42,7 +47,7 @@ class LustreOstMetricCollector(AbstractCollector):
         self.all_remote_ost_stats_so_far[remote_ost_dir_name] = remote_ost_stats_latest_value
         self.metrics_list_to_str()
         self.metrics_list_to_dict()
-            # return value_list, remote_ost_stats_latest_value
+        # return value_list, remote_ost_stats_latest_value
 
     def metrics_list_to_str(self):
         output_string = ""
@@ -60,3 +65,16 @@ class LustreOstMetricCollector(AbstractCollector):
             tmp_dict["{}{}".format(self.prefix, self.metrics_id_to_attr[keys_list[index]])] = self._get_data_type(
                 self.metrics_list[index], type_)
         self.metrics_dict = tmp_dict
+
+    def get_metrics_list_to_dict_no_prefix(self):
+        metrics_dict_no_prefix = {}
+        keys_list = list(self.metrics_id_to_attr.keys())
+        for index in range(len(self.metrics_list)):
+            type_ = self.metrics_datatypes[keys_list[index]]
+            metrics_dict_no_prefix[self.metrics_id_to_attr[keys_list[index]]] = self._get_data_type(
+                self.metrics_list[index], type_)
+        return metrics_dict_no_prefix
+
+    def get_proto_message(self):
+        message = ParseDict(self.get_metrics_list_to_dict_no_prefix(), LustreOstMetrics())
+        return message
