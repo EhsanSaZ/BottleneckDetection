8ce2f9968c520a0aacf8f58752b0b5b122491827
ehsan saeedizade <e24saeedi@gmail.com>
2022-07-22 17:29:07 -0700

    Implement get_proto_message method for system_metric_collector.py

--- dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/system_metric_collector.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/collectors/system_metric_collector.py
@@ -1,39 +1,41 @@
 import traceback
 from subprocess import Popen, PIPE
 import psutil
+from google.protobuf.json_format import ParseDict, MessageToDict
 
 try:
     from abstract_collector import AbstractCollector
+    from protobuf_messages.system.system_metrics_pb2 import SystemMetrics
 except ModuleNotFoundError:
     from .abstract_collector import AbstractCollector
-
+    from .protobuf_messages.system.system_metrics_pb2 import SystemMetrics
 
 class SystemMetricCollector(AbstractCollector):
     def __init__(self, prefix=""):
         super().__init__(prefix)
-        self.metrics_datatypes = {30: 'string', 31: 'string', 32: 'string', 33: 'string', 34: 'string', 35: 'string',
-                                  36: 'string', 37: 'string', 38: 'string', 39: 'string', 40: 'string', 41: 'string',
-                                  42: 'string', 43: 'string', 44: 'string', 45: 'string', 46: 'string', 47: 'string',
-                                  48: 'string', 49: 'string', 50: 'string', 51: 'string', 52: 'string', 53: 'string',
-                                  54: 'string', 55: 'string', 56: 'string', 57: 'string', 58: 'string', 59: 'intonly',
-                                  60: 'intonly', 61: 'intonly', 62: 'intonly', 63: 'string', 64: 'string', 65: 'string',
-                                  66: 'string', 67: 'string', 68: 'string', 69: 'string', 70: 'string', 71: 'string',
-                                  72: 'string', 73: 'string', 74: 'string', 75: 'string', 76: 'string', 77: 'string',
-                                  78: 'string', 79: 'string', 80: 'string', 81: 'string', 82: 'string', 83: 'string',
-                                  84: 'string', 85: 'string', 86: 'string', 87: 'string', 88: 'string'}
-        self.metrics_id_to_attr = {30: 'rchar', 31: 'wchar', 32: 'syscr', 33: 'syscw', 34: 'read_bytes_io',
-                                   35: 'write_bytes_io', 36: 'cancelled_write_bytes', 37: 'pid', 38: 'ppid', 39: 'pgrp',
-                                   40: 'session', 41: 'tty_nr', 42: 'tpgid', 43: 'flags', 44: 'minflt', 45: 'cminflt',
-                                   46: 'majflt', 47: 'cmajflt', 48: 'utime', 49: 'stime', 50: 'cutime', 51: 'cstime',
-                                   52: 'priority', 53: 'nice', 54: 'num_threads', 55: 'itrealvalue', 56: 'starttime',
-                                   57: 'vsize', 58: 'rss', 59: 'rsslim', 60: 'startcode', 61: 'endcode',
-                                   62: 'startstack', 63: 'kstkesp', 64: 'kstkeip', 65: 'signal', 66: 'blocked',
-                                   67: 'sigignore', 68: 'sigcatch', 69: 'wchan', 70: 'nswap', 71: 'cnswap',
-                                   72: 'exit_signal', 73: 'processor', 74: 'rt_priority', 75: 'policy',
-                                   76: 'delayacct_blkio_ticks', 77: 'guest_time', 78: 'cguest_time', 79: 'start_data',
-                                   80: 'end_data', 81: 'start_brk', 82: 'arg_start', 83: 'arg_end', 84: 'env_start',
-                                   85: 'env_end', 86: 'exit_code', 87: 'cpu_usage_percentage',
-                                   88: 'mem_usage_percentage'}
+        self.metrics_datatypes = {1: 'string', 2: 'string', 3: 'string', 4: 'string', 5: 'string', 6: 'string',
+                                  7: 'string', 8: 'string', 9: 'string', 10: 'string', 11: 'string', 12: 'string',
+                                  13: 'string', 14: 'string', 15: 'string', 16: 'string', 17: 'string', 18: 'string',
+                                  19: 'string', 20: 'string', 21: 'string', 22: 'string', 23: 'string', 24: 'string',
+                                  25: 'string', 26: 'string', 27: 'string', 28: 'string', 29: 'string', 30: 'intonly',
+                                  31: 'intonly', 32: 'intonly', 33: 'intonly', 34: 'string', 35: 'string', 36: 'string',
+                                  37: 'string', 38: 'string', 39: 'string', 40: 'string', 41: 'string', 42: 'string',
+                                  43: 'string', 44: 'string', 45: 'string', 46: 'string', 47: 'string', 48: 'string',
+                                  49: 'string', 50: 'string', 51: 'string', 52: 'string', 53: 'string', 54: 'string',
+                                  55: 'string', 56: 'string', 57: 'string', 58: 'string', 59: 'string'}
+        self.metrics_id_to_attr = {1: 'rchar', 2: 'wchar', 3: 'syscr', 4: 'syscw', 5: 'read_bytes_io',
+                                   6: 'write_bytes_io', 7: 'cancelled_write_bytes', 8: 'pid', 9: 'ppid', 10: 'pgrp',
+                                   11: 'session', 12: 'tty_nr', 13: 'tpgid', 14: 'flags', 15: 'minflt', 16: 'cminflt',
+                                   17: 'majflt', 18: 'cmajflt', 19: 'utime', 20: 'stime', 21: 'cutime', 22: 'cstime',
+                                   23: 'priority', 24: 'nice', 25: 'num_threads', 26: 'itrealvalue', 27: 'starttime',
+                                   28: 'vsize', 29: 'rss', 30: 'rsslim', 31: 'startcode', 32: 'endcode',
+                                   33: 'startstack', 34: 'kstkesp', 35: 'kstkeip', 36: 'signal', 37: 'blocked',
+                                   38: 'sigignore', 39: 'sigcatch', 40: 'wchan', 41: 'nswap', 42: 'cnswap',
+                                   43: 'exit_signal', 44: 'processor', 45: 'rt_priority', 46: 'policy',
+                                   47: 'delayacct_blkio_ticks', 48: 'guest_time', 49: 'cguest_time', 50: 'start_data',
+                                   51: 'end_data', 52: 'start_brk', 53: 'arg_start', 54: 'arg_end', 55: 'env_start',
+                                   56: 'env_end', 57: 'exit_code', 58: 'cpu_usage_percentage',
+                                   59: 'mem_usage_percentage'}
 
     def collect_metrics(self, pid_str, target_process):
         self.collect_system_metrics(pid_str, target_process)
@@ -114,3 +116,16 @@ class SystemMetricCollector(AbstractCollector):
             tmp_dict["{}{}".format(self.prefix, self.metrics_id_to_attr[keys_list[index]])] = self._get_data_type(
                 self.metrics_list[index], type_)
         self.metrics_dict = tmp_dict
+
+    def get_metrics_list_to_dict_no_prefix(self):
+        metrics_dict_no_prefix = {}
+        keys_list = list(self.metrics_id_to_attr.keys())
+        for index in range(len(self.metrics_list)):
+            type_ = self.metrics_datatypes[keys_list[index]]
+            metrics_dict_no_prefix[self.metrics_id_to_attr[keys_list[index]]] = self._get_data_type(
+                self.metrics_list[index], type_)
+        return metrics_dict_no_prefix
+
+    def get_proto_message(self):
+        message = ParseDict(self.get_metrics_list_to_dict_no_prefix(), SystemMetrics())
+        return message
