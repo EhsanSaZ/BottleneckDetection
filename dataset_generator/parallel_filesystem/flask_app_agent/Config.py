327d49f97c2c5df9bfc7d33ba80ab2bb590dcc76
ehsan saeedizade <e24saeedi@gmail.com>
2022-08-03 16:02:11 -0700

    Add Config file for http lustre metric collection

---
+++ dataset_generator/parallel_filesystem/flask_app_agent/Config.py
@@ -0,0 +1,6 @@
+class Config:
+    redis_cache_server_ip = "localhost"
+    redis_cache_server_port = "6379"
+    expire_ttl = 15
+    flask_app_agent_ip = "0.0.0.0"
+    flask_app_agent_port = "1234"
