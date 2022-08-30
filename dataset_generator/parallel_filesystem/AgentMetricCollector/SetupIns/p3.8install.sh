c174ce9c133fae95d9f0d566e233f7fd3441da5c
ehsan saeedizade <e24saeedi@gmail.com>
2022-08-30 11:34:26 -0700

    Add python3.8 on centos7 installation script

---
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/SetupIns/p3.8install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install openssl-devel bzip2-devel libffi-devel xz-devel
+sudo yum -y install wget
+wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
+tar xvf Python-3.8.12.tgz
+cd Python-3.8*/
+./configure --enable-optimizations
+sudo make altinstall
+sudo ln -fs /usr/local/bin/python3.8 /usr/bin/python3
