fed903246ce61b35d22021fc1c081511f1b410eb
ehsan saeedizade <e24saeedi@gmail.com>
2022-10-27 18:31:46 -0700

    Update dtn_io_metrics_dict in helper_threads.py

--- dataset_generator/parallel_filesystem/AgentMetricCollector/helper_threads.py
+++ dataset_generator/parallel_filesystem/AgentMetricCollector/helper_threads.py
@@ -5,20 +5,24 @@ import psutil
 
 from collectors.buffer_value_stat_collector import BufferValueStatCollector
 from collectors.resource_usage_collector import ResourceUsageCollector
+from collectors.DTN_io_metric_collector import DtnIoMetricsCollector
 # import system_monitoring_global_vars
 from multiprocessing import Process
 
 class globalMetricsMonitor(Process):
     def __init__(self, sleep_time, system_cpu_mem_usage_list, system_cpu_mem_usage_dict,
-                 system_buffer_value_list, system_buffer_value_dict, **kwargs):
+                 system_buffer_value_list, system_buffer_value_dict, client_io_metrics_dict, dtn_io_metrics_dict, **kwargs):
         super(globalMetricsMonitor, self).__init__(**kwargs)
         self.sleep_time = sleep_time
         self.resource_usage_collector = ResourceUsageCollector()
         self.buffer_value_collector = BufferValueStatCollector()
+        self.dtn_io_metric_collector = DtnIoMetricsCollector()
         self.system_cpu_mem_usage_list = system_cpu_mem_usage_list
         self.system_cpu_mem_usage_dict = system_cpu_mem_usage_dict
         self.system_buffer_value_list = system_buffer_value_list
         self.system_buffer_value_dict = system_buffer_value_dict
+        self.client_io_metrics_dict = client_io_metrics_dict
+        self.dtn_io_metrics_dict = dtn_io_metrics_dict
 
     def run(self):
         # global system_monitoring_global_vars.system_buffer_value, system_monitoring_global_vars.system_cpu_usage, system_monitoring_global_vars.system_memory_usage
@@ -51,6 +55,10 @@ class globalMetricsMonitor(Process):
             #
             # system_monitoring_global_vars.system_buffer_value_dict = self.buffer_value_collector.get_metrics_dict()
             # system_monitoring_global_vars.system_buffer_value_proto_message.CopyFrom(self.buffer_value_collector.get_proto_message())
+            self.dtn_io_metric_collector.collect_metrics(from_dict=self.client_io_metrics_dict)
+            self.dtn_io_metrics_dict['dtn_lustre_read_bytes'] = self.dtn_io_metric_collector.get_metrics_dict()["dtn_lustre_read_bytes"]
+            self.dtn_io_metrics_dict['dtn_lustre_write_bytes'] = self.dtn_io_metric_collector.get_metrics_dict()["dtn_lustre_write_bytes"]
+
             time.sleep(self.sleep_time)
 
 
